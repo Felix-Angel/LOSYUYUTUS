d957b4e18c60bb80bbf4c98e4c065be85c737727
Felix-Angel <felixangelgarcia79@gmail.com>
2025-04-02 11:46:56 -0600

    Se agrega al proeycto

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading;
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("¡Carrera de hilos!");
+
+        // Crear dos corredores
+        Thread corredorA = new Thread(Correr);
+        Thread corredorB = new Thread(Correr);
+
+        corredorA.Start("Corredor A");
+        corredorB.Start("Corredor B");
+
+        corredorA.Join();
+        corredorB.Join();
+
+        Console.WriteLine("¡Carrera terminada!");
+    }
+
+    static void Correr(object nombre)
+    {
+        Random rnd = new Random();
+        for (int pasos = 1; pasos <= 10; pasos++)
+        {
+            Console.WriteLine($"{nombre} avanzó a la posición: {pasos}");
+            Thread.Sleep(rnd.Next(100, 500)); // Velocidad aleatoria
+        }
+        Console.WriteLine($"🏁 {nombre} terminó la carrera!");
+    }
+}
