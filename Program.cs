74055d9124f3ed09054f88fe54ce082c0716669e
al3-dan <lopezcamarilloa@icloud.com>
2025-04-02 12:01:51 -0600

    se agrega el cuarto corredor

--- Program.cs
+++ Program.cs
@@ -1,23 +1,27 @@
 ﻿using System;
 using System.Threading;
+
 class Program
 {
     static void Main()
     {
         Console.WriteLine("¡Carrera de hilos!");
 
-        // Crear tres corredores
+        // Crear cuatro corredores
         Thread corredorA = new Thread(Correr);
         Thread corredorB = new Thread(Correr);
         Thread corredorC = new Thread(Correr);
+        Thread corredorD = new Thread(Correr);
 
         corredorA.Start("Corredor A");
         corredorB.Start("Corredor B");
         corredorC.Start("Corredor C");
+        corredorD.Start("Corredor D");
 
         corredorA.Join();
         corredorB.Join();
         corredorC.Join();
+        corredorD.Join();
 
         Console.WriteLine("¡Carrera terminada!");
     }
